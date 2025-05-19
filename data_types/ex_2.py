893af1412d8de88dccb6e894e474b574462a8083
EmRald Cheatum <Emraldc@yahoo.com>
2025-05-18 19:31:45 -0600

    Added Chapter 1 Exercises

---
+++ data_types/ex_2.py
@@ -0,0 +1,16 @@
+# Create a tuple called names that contains several pet names. For instance:
+
+# Name
+# Asta
+# Butterscotch
+# Pudding
+# Neptune
+# Darwin
+
+name = (
+    'Asta', 
+    'Butterscotch', 
+    'Pudding', 
+    'Neptune', 
+    'Darwin'
+)
